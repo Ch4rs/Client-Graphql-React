e3fc0aeffba124a65f6021ed95511b2f1b484b42
David Castro <campeador.david@gmail.com>
2022-02-14 17:09:56 -0700

    Documentacion

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,24 +6,30 @@ export default function Login() {
     <div class="column is-4 is-offset-4">
         <form>
             <div class="field">
-                <label class="label">Userame</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="Text input"/>
+                <p class="control has-icons-left has-icons-right">
+                    <input class="input" type="email" placeholder="Email"/>
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                    </span>
+                    <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                    </span>
+                </p>
                 </div>
-            </div>
-            <div class="field">
-                <label class="label">Password</label>
-                <div class="control">
-                    <input class="input" type="password" placeholder="Text input"/>
+                <div class="field">
+                <p class="control has-icons-left">
+                    <input class="input" type="password" placeholder="Password"/>
+                    <span class="icon is-small is-left">
+                    <i class="fas fa-lock"></i>
+                    </span>
+                </p>
                 </div>
-            </div>
-            <div class="field is-grouped">
-                <div class="control">
-                    <button class="button is-link">Submit</button>
-                </div>
-                <div class="control">
-                    <button class="button is-link is-light">Cancel</button>
-                </div>
+                <div class="field">
+                <p class="control">
+                    <button class="button is-success">
+                    Login
+                    </button>
+                </p>
             </div>
         </form>
     </div>
